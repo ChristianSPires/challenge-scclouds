a4317186806d510d532acbd3382dc5763330419e
Christian de Souza Pires <christianyourdan1@gmail.com>
2024-10-23 22:58:57 -0300

    fix: rename main files

--- Fibonacci/Linear/MainFibonacciLinear.java
+++ Fibonacci/Linear/MainFibonacciLinear.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 
-public class MainRecursive {
+public class MainFibonacciLinear {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n;
 
         System.out.print("Enter a number to get the index of it inside Fibonacci: ");
-        n = sc.nextInt();
+        int n = sc.nextInt();
 
-        int resp = FibonacciRecursive.getIndexInFibonacci(n);
+        int resp = FibonacciLinear.getIndexInFibonacci(n);
         System.out.println("fib(" + n + ") = " + resp);
     }
 }
--- PrimeNumbers/Linear/MainPrimeLinear.java
+++ PrimeNumbers/Linear/MainPrimeLinear.java
@@ -1,7 +1,7 @@
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class MainPrimeLinear {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -9,7 +9,7 @@ public class Main {
         System.out.print("Enter a number to get all prime numbers up to it: ");
         int n = sc.nextInt();
 
-        List<Integer> primeNumbers = PrimeNumberRecursive.getPrimesUpTo(n);
+        List<Integer> primeNumbers = PrimeNumberLinear.getPrimesUpTo(n);
         System.out.println("Prime numbers up to " + n + ": " + primeNumbers);
     }
 }
