02baebc3dd8bab6ff18ec163b34c31ee90bbbe82
Christian de Souza Pires <christianyourdan1@gmail.com>
2024-10-23 22:36:13 -0300

    feat (PrimeNumbersRecursive): showing every prime number up to input number

---
+++ PrimeNumbers/Recursive/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number to get all prime numbers up to it: ");
+        int n = sc.nextInt();
+
+        List<Integer> primeNumbers = PrimeNumberChecker.getPrimesUpTo(n, 2, new ArrayList<>());
+        System.out.println("Prime numbers up to " + n + ": " + primeNumbers);
+    }
+}
---
+++ PrimeNumbers/Recursive/PrimeNumberChecker.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+public final class PrimeNumberChecker {
+
+    private static boolean isPrime(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static List<Integer> getPrimesUpTo(int n, int current, List<Integer> primes) {
+        if (n < 2) {
+            throw new IllegalArgumentException("Must be greater than 1.");
+        }
+
+        if(current > n) {
+            return primes;
+        }
+
+        if(isPrime(current)) {
+            primes.add(current);
+        }
+
+        return getPrimesUpTo(n, current + 1, primes);
+    }
+}
