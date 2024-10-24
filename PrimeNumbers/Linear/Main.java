804c73ea73f3ff94203270a65a35ef694d8371ea
Christian de Souza Pires <christianyourdan1@gmail.com>
2024-10-23 22:35:41 -0300

    feat (PrimeNumbersLinear): showing every prime number up to input number

---
+++ PrimeNumbers/Linear/Main.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number to get all prime numbers up to it: ");
+        int n = sc.nextInt();
+
+        List<Integer> primeNumbers = PrimeNumberChecker.getPrimesUpTo(n);
+        System.out.println("Prime numbers up to " + n + ": " + primeNumbers);
+    }
+}
